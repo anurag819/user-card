2db0515023b001673d962bd759ca5b168e89c9e6
anurag819 <87742653+anurag819@users.noreply.github.com>
2022-06-23 23:24:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUser] = useState([]);
+
+  const loadUsers = async () => {
+    console.log("before");
+    const response = await fetch("https://api.github.com/users");
+    const jsonresponse = await response.json();
+    setUser(jsonresponse);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="head">
+        <h1>User Card</h1>
+        <button onClick={loadUsers}>Get Data</button>
+      </div>
+
+      <div className="data">
+        <h2>Users:</h2>
+        <ul>
+          {users.map(({ id, login, node_id }) => (
+            <li key={id}>
+              Name: {login}
+              <div>Node Id:{node_id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
